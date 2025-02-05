b03f595c67ebe1ce125f3565928d6b6dd41cbcec
anton <avainfoconseils@gmail.com>
2025-02-05 10:08:59 +0100

    feat: Correction of part 1

---
+++ M2I_ExempleCours/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using M2I_ExempleCours.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace M2I_ExempleCours.Controllers;
+
+public class ContactController : Controller
+{
+	private static List<Contact> _contacts = [];
+
+	// GET
+	public IActionResult Index()
+	{
+		return View(_contacts);
+	}
+
+	public IActionResult AddContact(string name, string email, string phoneNumber)
+	{
+		if (ModelState.IsValid
+		    && !(string.IsNullOrEmpty(name) && string.IsNullOrWhiteSpace(name))
+		    && !(string.IsNullOrEmpty(email) && string.IsNullOrWhiteSpace(email))
+		    && !(string.IsNullOrEmpty(phoneNumber) && string.IsNullOrWhiteSpace(phoneNumber))
+		   )
+		{
+			Contact c = new Contact { Id = _contacts.Count + 1, Name = name, Email = email, PhoneNumber = phoneNumber };
+			_contacts.Add(c);
+			foreach (var contact in _contacts)
+			{
+				Console.WriteLine(contact);
+			}
+		}
+
+		return RedirectToAction("Index");
+	}
+}
---
+++ M2I_ExempleCours/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace M2I_ExempleCours.Models;
+
+public class Contact
+{
+	public int Id { get; set; }
+	[MaxLength(50)] public string Name { get; set; } = "";
+	[MaxLength(255)] public string Email { get; set; } = "";
+	[MaxLength(9)] public string PhoneNumber { get; set; } = "";
+
+	public override string ToString()
+	{
+		return $"Id({Id}):\n\tName: {Name}\n\tEmail: {Email}\n\tPhoneNumber: {PhoneNumber}";
+	}
+}
